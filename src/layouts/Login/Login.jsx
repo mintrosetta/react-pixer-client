abb68919e3724262c930df4173e0999c9c5fa015
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-08-29 11:16:20 +0700

    add login page

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,7 +1,30 @@
+import styles from './Login.module.css';
+
 export default function Login() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Handle submited")
+    }
+
     return (
-        <div>
-            This is a Login layout !
+        <div className={styles.loginContainer}>
+            <div className={styles.loginCard}>
+                <h1 className={styles.loginHeader}>Authenticate</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" />
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" />
+                    </div>
+                    <div className={styles.loginButtonGroup}>
+                        <button type="submit" className={styles.loginBtn}>Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
