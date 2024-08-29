a4648e5e7c3e219b60d808695f599b215a9a62e9
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-08-29 11:40:02 +0700

    add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './layouts/Home/Home'
-import Login from './layouts/Login/Login'  
+import Login from './layouts/Login/Login'
+import Register from './layouts/Register/Register'
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <main>
         <BrowserRouter>
           <Routes>
-            <Route path='' element={ <Home /> } />
-            <Route path='/login' element={ <Login /> } />
+            <Route path='' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </BrowserRouter>
       </main>
--- src/layouts/Register/Register.jsx
+++ src/layouts/Register/Register.jsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/common/Form.module.css';
 
-export default function form() {
+export default function Register() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -10,7 +10,7 @@ export default function form() {
     return (
         <div className={styles.formContainer}>
             <div className={styles.formCard}>
-                <h1 className={styles.formHeader}>Authenticate</h1>
+                <h1 className={styles.formHeader}>Join with us</h1>
                 <form onSubmit={handleSubmit}>
                     <div className={styles.inputGroup}>
                         <label htmlFor="email">Email</label>
@@ -20,8 +20,16 @@ export default function form() {
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" name="password" />
                     </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input type="password" id="confirmPassword" name="confirmPassword" />
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" name="username" />
+                    </div>
                     <div className={styles.formButtonGroup}>
-                        <button type="submit" className={styles.formBtn}>Login</button>
+                        <button type="submit" className={styles.formBtn}>Register</button>
                     </div>
                 </form>
             </div>
